1a065a461f3619c4a28effee5089354562e39a69
xdherick <dherick60@hotmail.com>
2026-04-05 18:43:36 -0300

    Deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,22 +8,10 @@ const { errorHandler } = require('./middleware/error.middleware');
 const app = express();
 
 // Middlewares globais
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://cadastro-usuarios-phi-liard.vercel.app"
-];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS não permitido"));
-    }
-  },
-  credentials: true
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  credentials: true,
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
