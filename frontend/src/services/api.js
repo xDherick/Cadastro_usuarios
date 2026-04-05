907cfeec03ab38e910b954aebf54283c421e7768
xdherick <dherick60@hotmail.com>
2026-04-05 18:11:47 -0300

    Deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,6 @@
 // src/services/api.js
 import axios from 'axios';
+import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3001/api',
